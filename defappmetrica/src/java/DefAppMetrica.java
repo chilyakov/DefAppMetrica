286707cab8158a6e09115a4689bb6fe6631dbd6e
chilyakov <chilyakov@gmail.com>
2020-03-05 18:04:47 +0500

    Update DefAppMetrica.java

--- defappmetrica/src/java/DefAppMetrica.java
+++ defappmetrica/src/java/DefAppMetrica.java
@@ -12,8 +12,8 @@ public class DefAppMetrica {
 	public static void DefAppMetrica_setAppMetricaKey(final Activity appActivity, final String appMetricaKey) {
 		
 		YandexMetricaConfig config = YandexMetricaConfig.newConfigBuilder(appMetricaKey).build();		
-		YandexMetrica.activate(appActivity.getApplicationContext(), config);
-            	YandexMetrica.enableActivityAutoTracking(appActivity.getApplication());
+		YandexMetrica.activate(appActivity, config);
+            	YandexMetrica.enableActivityAutoTracking(appActivity);
 	}
 
 	public static void DefAppMetrica_trackEvent(Activity appActivity, String eventName, Map<String, Object> eventValue) {
